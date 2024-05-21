d506922a48acd4e52ce6770d84cb30d92f39d940
Abel Legesse <128784548+pogg-ab@users.noreply.github.com>
2024-05-21 01:33:34 -0700

    Add files via upload

---
+++ Solidity Address Interation Chapter 2/Chapter 2/sending ether/practice/1 Storing Owner/contract.sol
@@ -0,0 +1,13 @@
+//sending ether
+
+//contract.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+	address public owner;
+
+	constructor() {
+		owner = msg.sender;
+	}
+}
