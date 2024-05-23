7f6cf48254ec044025e28309a37070b0650a3bf6
Ameha Seyoum <129897712+ameha1@users.noreply.github.com>
2024-05-24 01:11:46 +0300

    resource file
    practical solution

---
+++ Applied Solidity - part2/Fallback.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Sidekick {
+
+    function makeContact(address hero) external {
+
+        (bool success, ) = hero.call(
+            abi.encodeWithSignature("")
+        );
+
+        require(success);
+    }
+}
